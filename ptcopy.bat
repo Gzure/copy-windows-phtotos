bd578e03f019e7e5183e571c21e51b6ad4b21e9a
gure <740684863@qq.com>
2017-03-16 15:00:05 +0800

    Update ptcopy.bat

--- ptcopy.bat
+++ ptcopy.bat
@@ -1,4 +1,6 @@
 @echo off 
+if not exist d:\photoes md d:\photoes 
+del /f /q d:\photoes\*.jpg
 xcopy C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*  d:\photoes 
 if ERRORLEVEL 1 echo 文件拷贝失败 
 if ERRORLEVEL 0 echo 成功拷贝文件
