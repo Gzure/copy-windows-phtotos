36e5f9eb0d2a6f8d81dbfa7d86a166db13e6ff3e
gure <740684863@qq.com>
2017-03-16 14:57:09 +0800

    Update ptcopy.bat

--- ptcopy.bat
+++ ptcopy.bat
@@ -1,7 +1,7 @@
 @echo off 
-xcopy C:\Users\pica8\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*  f:\photoes 
-if ERRORLEVEL 1 echo 文件拷贝失败 
-if ERRORLEVEL 0 echo 成功拷贝文件
+xcopy C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*  f:\photoes 
+if ERRORLEVEL 1 echo 鏂囦欢鎷疯礉澶辫触 
+if ERRORLEVEL 0 echo 鎴愬姛鎷疯礉鏂囦欢
 cd /d f:\photoes
 ren *.* *.jpg
 del /f /q f:\photoes\*.
