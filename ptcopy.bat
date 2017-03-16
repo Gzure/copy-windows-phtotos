dd718e81580d72087aa6e5ff4feb74356410a07a
gure <740684863@qq.com>
2017-03-16 14:58:46 +0800

    Update ptcopy.bat

--- ptcopy.bat
+++ ptcopy.bat
@@ -1,8 +1,8 @@
 @echo off 
-xcopy C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*  f:\photoes 
+xcopy C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*  d:\photoes 
 if ERRORLEVEL 1 echo 文件拷贝失败 
 if ERRORLEVEL 0 echo 成功拷贝文件
-cd /d f:\photoes
+cd /d d:\photoes
 ren *.* *.jpg
-del /f /q f:\photoes\*.
-start f:\test.vbs
+del /f /q d:\photoes\*.
+start d:\test.vbs
